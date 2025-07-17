248bb851e919ffe312d7af1875f4347ba25e0eea
nabilBoudarine <nabilboudarine@gmail.com>
2025-07-17 14:04:40 +0100

    add synthese table +correction de Hs bug

--- src/main/java/com/cmg/back/config/WebSecurityConfig.java
+++ src/main/java/com/cmg/back/config/WebSecurityConfig.java
@@ -22,9 +22,54 @@ public class WebSecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()  // <<--- Ouvre tout
-                        .anyRequest().permitAll()
+                        .requestMatchers(
+                                "/", "/home", "/home.html",
+                                "/signup", "/login",
+                                "/css/**", "/js/**", "/images/**",
+
+                                // Pages accessibles directement
+                                "/index.html",
+                                "/synthese.html",                    // ✅ synthèse
+                                "/cbulkArgentifere.html",
+                                "/chauxVive.html",
+                                "/expZincSafi.html",
+                                "/chauxViveLafarge.html",
+                                "/controle-bascule-hjds.html",
+                                "/controle-bascule-ka.html",
+                                "/dsClassique.html",
+                                "/dsNord.html",
+                                "/expZincOncf.html",
+                                "/expCuivreOncf.html",
+                                "/expCuivreNord.html",
+                                "/expPbCmgOnf.html",
+
+                                // API autorisées
+                                "/api/cbulkargentifere/**",
+                                "/api/chauxVive/**",
+                                "/api/chauxViveLafarge/**",
+                                "/api/bascule-hj-ds/**",
+                                "/api/ka/**",
+                                "/api/ds-classique/**",
+                                "/api/dsnord/**",
+                                "/api/expzinconcf/**",
+                                "/api/exp-zinc-safi/**",
+                                "/api/exp-cuivre-oncf/**",
+                                "/api/exp-cuivre-nord/**",
+                                "/api/exp-pb-cmg-onf/**",
+                                "/api/chaux-vive/**",
+
+                                // Synthèse API
+                                "/api/synthese-jour",
+                                "/api/synthese-mois",
+                                "/api/synthese-annuelle"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/home")
+                        .permitAll()
                 )
                 .build();
     }
+
 }
